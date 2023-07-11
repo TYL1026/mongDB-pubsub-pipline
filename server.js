643ca5901cbf7abc963e533effaf3f85e3322f0a
TonyL <65515982+TYL1026@users.noreply.github.com>
2023-07-10 22:05:48 -0700

    tcp test

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ const pubSubClient = new PubSub();
 const topic = pubSubClient.topic(process.env.PUB_SUB_TOPIC);
 console.log(topic.name)
 const topicName = process.env.PUB_SUB_TOPIC;
-const configDirectory = path.resolve(process.cwd(), "config");
-const file = fs.readFileSync(
-    path.join(configDirectory, "chang-stream-schema.avsc"),
-    "utf8"
-  );
-const definition = file.toString();
-const type = avro.parse(definition);
-
-
 topic.exists()
   .then(([exists]) => {
     if (exists) {
@@ -33,6 +24,17 @@ topic.exists()
   .catch((err) => {
     console.error('Error checking topic existence:', err);
   });
+
+const configDirectory = path.resolve(process.cwd(), "config");
+const file = fs.readFileSync(
+    path.join(configDirectory, "chang-stream-schema.avsc"),
+    "utf8"
+  );
+const definition = file.toString();
+const type = avro.parse(definition);
+
+
+
 
 const server = app.listen(port, () => {
     console.log(`Server Run on ${port} `)
