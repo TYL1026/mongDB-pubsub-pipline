a7d1459d13efb729b7484d2dbdb1313d1f00dc71
TonyL <65515982+TYL1026@users.noreply.github.com>
2023-07-10 20:27:50 -0700

    fix for GCP

--- server.js
+++ server.js
@@ -12,17 +12,13 @@ const port = process.env.PORT;
 const pubSubClient = new PubSub();
 const topic = pubSubClient.topic(process.env.PUB_SUB_TOPIC);
 const configDirectory = path.resolve(process.cwd(), "config");
-const PUB_SUB_TOPIC = 'projects/perceptive-day-388607/topics/MON_T';
+
 const file = fs.readFileSync(
     path.join(configDirectory, "chang-stream-schema.avsc"),
     "utf8"
   );
 const definition = file.toString();
 const type = avro.parse(definition);
-
-const server = app.listen(port, () => {
-    console.log(`Server Run on ${port} `)
-})
 
 
 const connectDb = async () => {
