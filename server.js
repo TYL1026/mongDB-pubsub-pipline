cc1e1379dfabc74cb48b626d82a88cf0ce4a2a8c
TonyL <65515982+TYL1026@users.noreply.github.com>
2023-07-10 21:49:05 -0700

    gcp update

--- server.js
+++ server.js
@@ -38,17 +38,17 @@ async function monitorCollectionForInserts(client, databaseName, collectionName)
     const collection = client.db(databaseName).collection(collectionName);
     // An aggregation pipeline that matches on new documents in the collection.
     const changeStream = collection.watch([], { fullDocument: 'updateLookup' });
-    // changeStream.on('change', event => {
-    //     const document = event.fullDocument;
-    //     if(event.operationType != 'delete'){
-    //         console.log(document)
-    //         publishDocumentAsMessage(document );
-    //         console.log("New Row added")
-    //     }else{
-    //         console.log("Row deleted")
-    //     }
+    changeStream.on('change', event => {
+        const document = event.fullDocument;
+        if(event.operationType != 'delete'){
+            console.log(document)
+            publishDocumentAsMessage(document );
+            console.log("New Row added")
+        }else{
+            console.log("Row deleted")
+        }
         
-    // });
+    });
  }
 
  async function publishDocumentAsMessage(document) {
